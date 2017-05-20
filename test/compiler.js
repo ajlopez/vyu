6f831fab1b95d8858996ec6cadf50f5f10c4da21
ajlopez <webmaster@ajlopez.com>
2017-05-20 17:53:15 -0300

    Compile using property and function

--- test/compiler.js
+++ test/compiler.js
@@ -13,3 +13,15 @@ exports['compile using model property'] = function (test) {
 	test.equal(result, 'Adam');
 };
 
+exports['compile using model property and function'] = function (test) {
+	var fn = compiler.compile('name.toUpperCase()');
+	
+	test.ok(fn);
+	test.equal(typeof fn, 'function');
+	
+	var result = fn({ name: 'Adam' });
+	
+	test.ok(result);
+	test.equal(result, 'ADAM');
+};
+
