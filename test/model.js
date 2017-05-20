dacd4493474b14460205b057d43d969f2ff960b4
ajlopez <webmaster@ajlopez.com>
2017-05-20 19:07:57 -0300

    Model using handler and proxy

--- test/model.js
+++ test/model.js
@@ -15,4 +15,34 @@ exports['create model from function and get properties'] = function (test) {
 	test.equal(result.name, 'Adam');
 	test.equal(result.age, 800);
 };
- 
+
+exports['create model with handler and get properties'] = function (test) {
+	var visited = {};
+	
+	var result = model({ 
+		name: 'Adam', 
+		age: 800 
+		},
+		{
+			handler: {
+				get: function (target, name) {
+					visited[name] = true;
+					return target[name];
+				}
+			}
+		});
+	
+	test.ok(result);
+	
+	test.ok(!visited.name);
+	test.ok(!visited.age);
+	
+	test.equal(result.name, 'Adam');
+	test.ok(visited.name);
+	test.ok(!visited.age);
+
+	test.equal(result.age, 800);
+	test.ok(visited.name);
+	test.ok(visited.age);
+};
+
